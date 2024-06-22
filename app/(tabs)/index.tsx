ab7f3427de661b2f0d6ecce8dfbf7545d3947c79
Alhussain <omarh3795@gmail.com>
2024-06-22 15:11:32 +0300

    removing unneccassry code

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,10 +2,6 @@ import React from "react";
 import {
   StyleSheet,
   View,
-  ScrollView,
-  Image,
-  Text,
-  TouchableOpacity,
 } from "react-native";
 import { ThemedText } from "@/components/ThemedText";
 import { ThemedView } from "@/components/ThemedView";
